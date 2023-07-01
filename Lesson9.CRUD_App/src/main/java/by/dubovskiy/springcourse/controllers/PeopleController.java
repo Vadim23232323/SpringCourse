5152ac9738a25beced3ec3e4b606ce11570d954a
Vadim <Vadim_milo.net@mail.ru>
2023-07-01 10:30:13 +0300

    CRUD, REST, Паттерн DAO (Data Access Object).

--- Lesson9.CRUD_App/src/main/java/by/dubovskiy/springcourse/controllers/PeopleController.java
+++ Lesson9.CRUD_App/src/main/java/by/dubovskiy/springcourse/controllers/PeopleController.java
@@ -1,2 +1,39 @@
-package by.dubovskiy.springcourse.controllers;public class PeopleController {
+package by.dubovskiy.springcourse.controllers;
+
+import by.dubovskiy.springcourse.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+
+
+    @GetMapping()
+    public String index(Model model){
+
+        // Получим всех людей из DAO и передадим на отображение в представление
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // Получим одного человека из DAO и передадим на отображение в представление
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
 }
--- Lesson9.CRUD_App/src/main/java/by/dubovskiy/springcourse/dao/PersonDAO.java
+++ Lesson9.CRUD_App/src/main/java/by/dubovskiy/springcourse/dao/PersonDAO.java
@@ -1,2 +1,31 @@
-package by.dubovskiy.springcourse.dao;public class PersonDAO {
+package by.dubovskiy.springcourse.dao;
+
+import by.dubovskiy.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT,"Dima"));
+        people.add(new Person(++PEOPLE_COUNT,"Maksim"));
+        people.add(new Person(++PEOPLE_COUNT,"Tanya"));
+    }
+
+    // Получаем весь список людей
+    public List<Person> index() {
+        return people;
+    }
+
+    // Получаем одного человека
+    public Person show(int id) {
+        return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
+    }
 }
